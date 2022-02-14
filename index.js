cb001c4f5c9d8b1a492478af7ac336df5f01632e
Alisa Gezalova <alisa.gezalova@gmail.com>
2022-02-13 18:33:36 -0700

    commit tree 2c27e0502b8d09ff9f902c01090abf75f37ecaf3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+function superbowlWin(record) {
+    let winYear = record.find(record => record.result === "W")
+    if (winYear) {
+        return winYear.year
+    } else {
+        return undefined
+    }
+    }
+
+ //Additional notes: record is an array of objects (each element {year: "2000", result: "W"} is an object). In let winYear = record.find(record => record.result === "W") is basically array.find(object => object.property === "W")   
